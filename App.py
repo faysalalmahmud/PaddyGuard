8b921761f048e1a9756c7f0b42480c5e1546604f
faysalalmahmud <[faysalalmahmud78@gmail.com]>
2025-06-24 18:52:04 +0600

    footer updated

--- App.py
+++ App.py
@@ -47,7 +47,7 @@ def main():
     st.title("🌾 Paddy Guard")
     st.subheader('A Deep Learning Model for Paddy Leaf Disease Detection and Classification')
     st.write("Upload an image of a paddy leaf to detect and classify diseases.")
-    st.footer("Developed by Faysal Al Mahmud")  # Footer with developer's name
+    st.footer("Developed by [Faysal Al Mahmud]")  # Footer with developer's name
 
     # Load the model using the cached function
     model = load_model()
